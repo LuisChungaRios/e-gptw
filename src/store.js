f9015f5996dbe162bf38b859ce421f765ea3fa7c
= <=>
2019-08-21 03:17:34 -0500

    add data

--- src/store.js
+++ src/store.js
@@ -137,131 +137,201 @@ El robot aprende en base a algoritmos para calificar cuáles curriculum son "bue
         ]
       }
     ],
-    inspirando: [
+    hablando: [
     
       {
-        name: 'TRABAJA CON NOSOTROS',
-        title: 'TRABAJA CON NOSOTROS',
-        content: "En Entel nos mueve la pasión por todo lo que hacemos y lo transmitimos al poner lo\n" +
-          "mejor de nosotros de manera dinámica, innovadora, disruptiva y digital. Por esta\n" +
-          "razón, desde 2018 mejoramos nuestro proceso de reclutamiento y selección,\n" +
-          "reduciéndolo de 21 a 18 días. De esta manera, atendemos a nuestros clientes y\n" +
-          "ponemos énfasis en seleccionar a colaboradores que sientan el latir de Entel con skills\n" +
-          "que encajen con nuestra Cultura, basándonos en nuestros 5 Pilares Culturales (mayor\n" +
-          "detalle en la sección Inspiración):",
+        name: 'Comunícate',
+        title: 'Comunícate',
+        content: `En este espacio de comunicación, Ramiro Lafarga . Gerente General, se encarga de
+        transmitir de una forma claro y directa toda la información clave relacionada al
+        negocio y a todos los líderes de Entel. Además, contamos con la presentación de los
+        Vicepresidentes y Gerentes Centrales quienes comparten los avances respectivos a
+        sus equipos, demostrando así que son parte de la solución. Este evento tiene
+        frecuencia trimestral y una asistencia aproximada de 270 líderes.`,
         links: {
-          documents: 'https://issuu.com/entellovers/docs/1trabaja_con_nosotros_web.pptx',
+          documents: 'https://issuu.com/hablando.elovers/docs/comun_cate_entel.pptx',
+          videos: 'https://youtu.be/oPqdMjyXOgw',
+          otherFiles: '#'
+        
+        },
+        list: [
+         {}
+        ]
+      },
+      {
+        name: 'Staff de gerentes',
+        title: 'Staff de gerentes',
+        content: `Mes a mes y con el liderazgo que los caracteriza, los gerentes se reúnen para revisar
+        los avances progresivos del negocio y rubro de las telecomunicaciones. Ellos, con un
+        mensaje claro y directo, desarrollan nuevas mejoras para el equipo.`,
+        links: {
+          videos:'#',
+          documents: 'https://issuu.com/hablando.elovers/docs/staff_de_gerentes.pptx',
+          otherFiles: '#'
+        },
+        list: [
+            {}
+        ]
+      },
+      {
+        name: `Planeamiento de Gerentes`,
+        title: 'Planeamiento de Gerentes',
+        content: `Un buen líder comparte su experiencia y conocimientos para así, en conjunto,
+        experimentar y cambiar hacia un mejor desempeño. Esta es la señal que transmiten
+        los gerentes directos y vicepresidentes Entel hacia sus equipos de trabajo. De este
+        modo el latido del Entel Lover resuena en los colaboradores. Este año, el
+        planeamiento se realizó el hotel Costa del Sol en Trujillo, del 16 al 18 de enero.`,
+      
+        links: {
+          documents: 'https://issuu.com/hablando.elovers/docs/planeamiento_de_gerentes.pptx',
+          videos:'#',
+          otherFiles: '#'
+        } ,
+        list: [
+         {}
+      
+        ]
+      },
+      {
+        name: `Reuniones por gerencia`,
+        title: 'Reuniones por gerencia',
+        content: `Luego del Staff de Gerentes, cada líder realiza una reunión con su gerencia para bajar
+        la información estratégica del negocio. Asimismo, también se utiliza este espacio para
+        discutir temas propios de la Gerencia y así mantener a todo el equipo alineado e
+        informado.
+        Cada gerencia decide la agenda de este espacio, para así mantener carta abierta
+        cualquier tema relacionado al trabajo que deseen compartir. En este espacio también
+        se realizan reconocimientos a colaboradores que hayan tenido un desempeño
+        sobresaliente durante el mes.`,
+        links: {
+          documents: 'https://issuu.com/hablando.elovers/docs/reuniones_por_gerencia.pptx',
+          videos: '#',
+          otherFiles: '#'
+        },
+        list: [
+          {}
+        ]
+      },
+      {
+        name: 'Reuniones comerciales en el cine',
+        title: 'Reuniones comerciales en el cine',
+        content: `En Entel experimentamos con nuevas formar de comunicar a nuestro equipo los
+        indicadores de negocio y las metas alcanzadas. Y para hacer más ágil y dinámica la
+        actividad, cambiamos la manera de contarlo, por este motivo los mercados se reúnen
+        una vez al mes; pero no de una reunión tradicional, ¡Se realiza en el cine! Cada
+        encuentro tiene una temática de película o serie y se premia a los Top Performers del
+        mes. Además, se invita a socios de negocio como Huawei o LG, los cuales presentan
+        las últimas novedades en tecnología.`,
+        links: {
+          documents: 'https://issuu.com/hablando.elovers/docs/reuniones_comerciales.pptx',
+          videos:'#',
+          otherFiles: '#'
+        },
+        list: [
+      
+        ]
+      }
+    ],
+    inspirando: [
+      {
+        name: 'Nuevo proposíto',
+        title: 'Nuevo proposíto',
+        content: `Existimos para acercar las infinitas posibilidades que da la tecnología y así transformar responsablemente la sociedad`,
+        links: {
+          documents: 'https://issuu.com/entellovers/docs/sin_t_tulo-1',
           videos: '#',
           otherFiles: '#'
         
         },
         list: [
-          {
-            name: 'Liderazgo Entel',
-          
-          },
-          {
-            name: 'Hazlo Increible'
-          },
-          {
-            name: 'Experimenta y Cambia'
-          },
-          {
-            name: 'Se parte de la Solución'
-          },
-          {
-            name: 'Siente como tu cliente'
-          }
+         {}
         ]
       },
       {
-        name: 'ENTREVISTA POR VIDEO LLAMADA ENTELINA',
-        title: 'ENTREVISTA POR VIDEO LLAMADA ENTELINA',
-        content: `Los candidatos deben llegar a conocernos tanto como nosotros a ellos y qué mejor a través del medio que nosotros ofrecemos. Esa es la premisa de nuestra Cultura que se refleja en cada interacción con ellos durante todo el proceso conectando con sus intereses y motivaciones a través de señales que inspiran y les pedimos que nos envíen un video por WhatsApp, contándonos quiénes son y qué es lo que les motiva a trabajar con nosotros.Cada nuevo candidato debe pasar por las siguientes instancias:
-`,
+        name: 'Nuevos pilares culturales',
+        title: 'Nuevos pilares culturales',
+        content: `La nueva cultura también se transformó, junto con los pilares de nuestra empresa:`,
         links: {
-          videos:'https://www.youtube.com/playlist?list=PLGy3YHNBg96NfPWPOHIIyl9NhZJY1R23T',
-          documents: '#',
+          videos:'https://youtu.be/M6XBiMy5maI',
+          documents: 'https://issuu.com/inspirando.elovers/docs/nuevos_pilares.pptx',
           otherFiles: '#'
         },
         list: [
-          {
-            name: `Video de presentación`
-          },
-          {
-            name: `Entrevista con el área solicitante`
-          },
-          {
-            name: `Entrevista personal con el área de Atracción del Talento`
-          },
-          {
-            name: `Dinámica de selección`
-          },
-          {
-            name: `Evaluación psicotécnica`
-          },
-          {
-            name: `Assessment`
-          },
-          {
-            name: `Verificación académica, laboral y antecedentes penales, policiales, web y redes sociales`
-          },
-          {
-            name: `Examen médico pre - ocupacional`
-          }
-      
-      
+            {}
         ]
       },
       {
-        name: `UN NUEVO TALENTO SE INCORPORA AL EQUIPO. ¿YA ESTÁS LISTO?`,
-        title: 'UN NUEVO TALENTO SE INCORPORA AL EQUIPO. ¿YA ESTÁS LISTO?',
-        content: `Esta práctica incorpora una nueva forma de apoyar a los líderes de Entel a gestionar sus equipos desde el inicio. Se envía un flyer a los jefes de equipos cada vez que un nuevo ingreso se incorpora. En este flyer se encuentran los tips que necesitan los jefes para acompañar el Onboarding del nuevo colaborador y así asegurar que éste viva la mejor experiencia desde su ingreso, propiciando el inicio de una conexión transformadora. En cuanto a nuestros diversos canales de reclutamiento, presentamos:`,
+        name: `Historias que inspiran (El reconocimiento de nuestros clientes)`,
+        title: 'Historias que inspiran (El reconocimiento de nuestros clientes)',
+        content: `Para nosotros es muy importante que nuestros colaboradores estén orgullosos de
+        representarnos y que se identifiquen como Entel Lovers. Así buscan la mejora
+        continua y el crecimiento de la organización, como también se preocupan por
+        desarrollar su talento y brindan siempre una experiencia distintiva al cliente, ya sea
+        interna o externa.
+        Para lograr que nuestros colaboradores compartan emociones con nuestros clientes,
+        nosotros, como marca, somos los que damos el primer paso, inspirándolos y logrando
+        emociones en ellos para que luego puedan compartirlas con nuestros clientes.
+        El colaborador Entel entiende lo fundamental de todo esto, se preocupa también por
+        comprender las necesidades del cliente, por lo que genera soluciones innovadoras,
+        competitivas y que establezcan relaciones de largo plazo. A través de un mensaje:
+        simple, cercano, confiable y extraordinario, enviará una señal transformadora que se
+        convertirá en un latido y finalmente en un cliente fidelizado y que ama la marca: un
+        Entel Lover.
+        Y estamos seguros que este es el camino gracias a las cartas de agradecimiento y
+        felicitación que recibimos diariamente de nuestros clientes hacia los colaboradores. Y
+        aunque somos amantes de la tecnología, somos aún más amantes de las cartas de
+        puño y letra de nuestros clientes. Y, si bien la primera vez nos sorprendió, ahora ya
+        tenemos un espacio mensual destinado a leer en voz alta las cartas que recibimos,
+        dando un gran aplauso a todos aquellos que lograron convertir a nuestros clientes en
+        auténticos Entel Lovers.`,
       
         links: {
-          documents: 'https://issuu.com/entellovers/docs/2_un_nuevo_talento_se_incorpora_a_tu_equipo.pptx',
-          videos:'#',
+          documents: '#',
+          videos:'https://youtu.be/-S1wJuU06qM',
           otherFiles: '#'
         } ,
         list: [
-          {
-            name: `Postulaciones internas:`,
-          
-          },
-          {
-            name: `Referidos`
-          },
-          {
-            name: `Postulaciones Externas`
-          }
+         {}
       
         ]
       },
       {
-        name: `AIRA - PLATAFORMA DE RECLUTAMIENTO `,
-        title: 'AIRA - PLATAFORMA DE RECLUTAMIENTO',
-        content: `Desde este año 2019, contamos con una nueva plataforma que nos ayuda a reducir nuestro proceso de selección con ayuda de inteligencia artificial.
-
-AIRA es un software de inteligencia artificial que tiene la capacidad de filtrar y seleccionar miles de currículum en segundos dependiendo de las características de la persona que una empresa necesita.
-
-El robot aprende en base a algoritmos para calificar cuáles curriculum son "buenos" o "malos" dependendiendo de las habilidades que tiene la persona que se necesita. Luego `,
+        name: `Entel + (Desafío Entel)`,
+        title: 'Entel + (Desafío Entel)',
+        content: `Entel+ es un programa regional (Chile-Perú) que busca empoderar a los
+        colaboradores a liderar iniciativas disruptivas que generen mayor valor a la compañía y
+        en sus tareas diarias. Buscar convertirlos en líderes de la transformación para
+        construir juntos el Entel del futuro.
+        Los participantes son considerados dueños de las iniciativas desde la etapa de
+        planificación hasta la ejecución con un acompañamiento continuo de la Oficina de
+        Transformación, lo que les permite sentirse parte de la solución y el cambio.`,
         links: {
-          documents: 'https://issuu.com/entellovers/docs/3aira.pptx',
-          videos: 'https://youtu.be/b2nF2z6d3tI',
+          documents: ' https://issuu.com/inspirando.elovers/docs/entel__.ppt',
+          videos: 'https://www.youtube.com/playlist?list=PLGy3YHNBg96OgygEJIbAxpW5wdLLrSQVb',
           otherFiles: '#'
         },
         list: [
-      
+          {}
         ]
       },
       {
-        name: 'TUS PRIMEROS DÍAS EN ENTEL ',
-        title: 'TUS PRIMEROS DÍAS EN ENTEL ',
-        content: `
-        Como parte de nuestro proceso de bienvenida, incluimos una nueva buena práctica: hacer seguimiento de tus primeros días en la empresa. Enviamos una breve encuesta sobre cómo se están sintiendo hasta el momento. Esto ayuda a iniciar el camino de relación entre cada Entel Lover con Entel y así se empieza a sentir el latido.`,
+        name: 'Entel lovers',
+        title: 'Entel lovers',
+        content: `¿Quién es un Entel Lover? Si tenemos que explicarlo, podemos decir que es una
+        filosofía que integra a toda nuestra organización para trabajar iniciativas pro cliente
+        que aseguran una experiencia de calidad y satisfacción total de cara a nuestros
+        colaboradores como a nuestros clientes-usuarios. Además, un Entel Lover es un
+        conocedor de la marca, que a través de un mensaje simple, expresivo y transformador,
+        se convierte en una señal para los usuarios y sus propios compañeros, inspirándolos y
+        convirtiéndolos en nuevos Entel Lovers. Un Entel Lover es un cliente y también un
+        colaborador Entel.
+        Para lograr estas acciones es que trabajamos de manera transversal a toda la
+        compañía con la participación de las diferentes áreas, en el desarrollo de acciones,
+        iniciativas y estrategias enfocadas en 4 drivers: Servicio al Cliente, Marca, Cultura y
+        Colaboradores`,
         links: {
-          documents: 'https://issuu.com/entellovers/docs/4tus_primeros_d_as_entel.pptx',
-          videos:'#',
+          documents: 'https://issuu.com/inspirando.elovers/docs/entel_lovers.pptx',
+          videos:'https://youtu.be/7Mf8_GzW9BA',
           otherFiles: '#'
         },
         list: [
