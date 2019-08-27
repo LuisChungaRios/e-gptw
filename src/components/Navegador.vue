ef1d505329670fd066304100972b57177ebf3ba7
Joan_C <joan0492@gmail.com>
2019-08-27 13:02:33 -0500

    creo que salio el menu :V

--- src/components/Navegador.vue
+++ src/components/Navegador.vue
@@ -2,7 +2,7 @@
     <div id="navegador" class="lista">
         <ul>
             <li class="btn-ancla">
-                <a href="#section-1" class="nav-link">
+                <a href="#section-1" class="nav-link section-1">
                     <img src="img/corazon/heart1.png" alt="corazon_lleno" class="corazon_lleno">
                     <div class="texto">
                         <span class="numero">1.</span>
@@ -10,8 +10,9 @@
                     </div>
                 </a>
             </li>
+            <li style="display: none;"><a href="#intermedio1" class="intermedio1"></a></li>
             <li class="btn-ancla">
-                <a href="#inspirando" class="nav-link">
+                <a href="#inspirando" class="nav-link inspirando">
                     <img src="img/corazon/heart2.png" alt="corazon_lleno" class="corazon_lleno">
                     <div class="texto">
                         <span class="numero">2.</span>
@@ -19,8 +20,10 @@
                     </div>
                 </a>
             </li>
+            <li style="display: none;"><a href="#inspirando-des" class="inspirando-des"></a></li>
+            <li style="display: none;"><a href="#intermedio2" class="intermedio2"></a></li>
             <li class="btn-ancla">
-                <a href="#hablando" class="nav-link">
+                <a href="#hablando" class="nav-link hablando">
                     <img src="img/corazon/heart3.png" alt="corazon_lleno" class="corazon_lleno">
                     <div class="texto">
                         <span class="numero">3.</span>
@@ -28,8 +31,10 @@
                     </div>
                 </a>
             </li>
+            <li style="display: none;"><a href="#hablando-des" class="hablando-des"></a></li>
+            <li style="display: none;"><a href="#intermedio3" class="intermedio3"></a></li>
             <li class="btn-ancla">
-                <a href="#escuchando" class="nav-link">
+                <a href="#escuchando" class="nav-link escuchando">
                     <img src="img/corazon/heart4.png" alt="corazon_lleno" class="corazon_lleno">
                     <div class="texto">
                         <span class="numero">4.</span>
@@ -37,8 +42,10 @@
                     </div>
                 </a>
             </li>
+            <li style="display: none;"><a href="#escuchando-des" class="escuchando-des"></a></li>
+            <li style="display: none;"><a href="#intermedio4" class="intermedio4"></a></li>
             <li class="btn-ancla">
-                <a href="#agradeciendo" class="nav-link">
+                <a href="#agradeciendo" class="nav-link agradeciendo">
                     <img src="img/corazon/heart5.png" alt="corazon_lleno" class="corazon_lleno">
                     <div class="texto">
                         <span class="numero">5.</span>
@@ -46,35 +53,43 @@
                     </div>
                 </a>
             </li>
+            <li style="display: none;"><a href="#agradeciendo-des" class="agradeciendo-des"></a></li>
+            <li style="display: none;"><a href="#intermedio5" class="intermedio5"></a></li>
             <li class="btn-ancla">
-                <a href="#desarrollando" class="nav-link">
+                <a href="#desarrollando" class="nav-link desarrollando">
                     <img src="img/corazon/heart6.png" alt="corazon_lleno" class="corazon_lleno">
                     <div class="texto">
                         <span class="numero">6.</span>
                         <span class="descripcion">Desarrollando</span>
                     </div>
                 </a>
-            </li>  
+            </li>
+            <li style="display: none;"><a href="#desarrollando-des" class="desarrollando-des"></a></li>
+            <li style="display: none;"><a href="#intermedio6" class="intermedio6"></a></li> 
             <li class="btn-ancla">
-                <a href="#balanceando" class="nav-link">
+                <a href="#balanceando" class="nav-link balanceando">
                     <img src="img/corazon/heart7.png" alt="corazon_lleno" class="corazon_lleno">
                     <div class="texto">
                         <span class="numero">7.</span>
                         <span class="descripcion">Balanceando</span>
                     </div>
                 </a>
-            </li>  
+            </li>
+            <li style="display: none;"><a href="#balanceando-des" class="balanceando-des"></a></li>
+            <li style="display: none;"><a href="#intermedio7" class="intermedio7"></a></li>
             <li class="btn-ancla">
-                <a href="#celebrando" class="nav-link">
+                <a href="#celebrando" class="nav-link celebrando">
                     <img src="img/corazon/heart8.png" alt="corazon_lleno" class="corazon_lleno">
                     <div class="texto">
                         <span class="numero">8.</span>
                         <span class="descripcion">Celebrando</span>
                     </div>
                 </a>
-            </li> 
+            </li>
+            <li style="display: none;"><a href="#celebrando-des" class="celebrando-des"></a></li>
+            <li style="display: none;"><a href="#intermedio8" class="intermedio8"></a></li>
             <li class="btn-ancla">
-                <a href="#reconociendo" class="nav-link">
+                <a href="#reconociendo" class="nav-link reconociendo">
                     <img src="img/corazon/heart9.png" alt="corazon_lleno" class="corazon_lleno">
                     <div class="texto">
                         <span class="numero">9.</span>
